545744d95a9e51ab7ead4cfa8fe50d55c9e0f719
Josh White <whitejosh429@gmail.com>
2025-01-12 17:17:45 +0000

    href

--- backend/nav.php
+++ backend/nav.php
@@ -1,6 +1,6 @@
 <div class="nav">
     <a class="button" href="/pages/">Home</a>
-    <a class="button" href="/pages/discography">Discography</a>
+    <a class="button" href="/pages/discography/">Discography</a>
     <a class="button" href="/pages/contact.php">Contact</a>
     <a class="button" href="/pages/sotd.php">Song Of The Day</a>
 </div>
