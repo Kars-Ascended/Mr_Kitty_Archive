e4360e8e26ad1661b0bf8abf3bcb8d0be9c75ea3
Josh White <whitejosh429@gmail.com>
2025-01-14 23:17:17 +0000

    htaccess changes to fix /pages in address bar, however does not work with subdirs yet

--- backend/nav.php
+++ backend/nav.php
@@ -1,6 +1,6 @@
 <div class="nav">
-    <a class="button" href="/pages/">Home</a>
-    <a class="button" href="/pages/discography/">Discography</a>
-    <a class="button" href="/pages/contact.php">Contact</a>
-    <a class="button" href="/pages/sotd.php">Song Of The Day</a>
+    <a class="button" href="/home">Home</a>
+    <a class="button" href="/discography/">Discography</a>
+    <a class="button" href="/contact">Contact</a>
+    <a class="button" href="/song-of-the-day">Song Of The Day</a>
 </div>
