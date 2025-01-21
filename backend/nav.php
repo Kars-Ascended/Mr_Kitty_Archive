b1832770add716c8fd6c71a2fc94546b506aacb4
Josh White <whitejosh429@gmail.com>
2025-01-21 21:54:34 +0000

    nav img fix attempts

--- backend/nav.php
+++ backend/nav.php
@@ -4,8 +4,9 @@
     <a class="button" href="/contact">Contact</a>
     <a class="button" href="/song-of-the-day">Song Of The Day</a>
 
-    <a href="/home"><img src="/assets\images\logo2.png"></a> <!-- Home -->
 </div>
+
+<a href="/home"><img src="/assets\images\logo2.png"></a> <!-- Home -->
 
 <!-- places the corner forrest img -->
 <img class="corner" src="\assets\images\blue-tsp.png">
