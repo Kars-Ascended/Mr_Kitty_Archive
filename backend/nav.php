5d7de8622ba12f3bd34051ba2b5e077c78d62a59
Josh White <whitejosh429@gmail.com>
2025-02-20 01:37:54 +0000

    removed lyrics page from nav

--- backend/nav.php
+++ backend/nav.php
@@ -1,6 +1,5 @@
 <div class="nav">
     <a class="button" href="/home">Home</a>
     <a class="button" href="/discography/">Discography</a>
-    <a class="button" href="/lyrics">Lyrics</a>
     <a class="button" href="/daily-song">Daily song</a>
 </div>
