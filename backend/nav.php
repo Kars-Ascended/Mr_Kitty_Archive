2b1b3d6c7a9523b584c82c84d5fdcc463cd395e5
Josh White <whitejosh429@gmail.com>
2025-02-26 22:52:38 +0000

    added contact to navbar

--- backend/nav.php
+++ backend/nav.php
@@ -2,5 +2,6 @@
     <a class="button" href="/home">Home</a>
     <a class="button" href="/discography/">Discography</a>
     <a class="button" href="/gallery">Gallery</a>
+    <a class="button" href="/contact">Contact</a>
     <a class="button" href="/daily-song">Daily song</a>
 </div>
