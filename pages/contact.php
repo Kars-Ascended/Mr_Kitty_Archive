01863abb145b7eddb550bcaa420f2bfa6ce203fc
Josh White <whitejosh429@gmail.com>
2025-02-26 22:52:02 +0000

    added contact page

---
+++ pages/contact.php
@@ -0,0 +1,13 @@
+<head>
+    <title>Contact</title>
+    <?php include "../backend/nav.php" ?> 
+    <link rel="stylesheet" href="/css/base.css">
+    <link rel="stylesheet" href="/css/meta.css">
+</head>
+<body>
+    <div class="main">
+        <p>test<p>
+
+    </div>
+</body>
+</html>
