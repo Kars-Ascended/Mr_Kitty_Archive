dacd14d05176cfa1d889072fb840ca706279c62b
Josh White <whitejosh429@gmail.com>
2025-02-28 03:15:28 +0000

    added updates to nav

--- backend/nav.php
+++ backend/nav.php
@@ -1,6 +1,7 @@
 <div class="nav">
     <a class="button" href="/home">Home</a>
     <a class="button" href="/discography/">Discography</a>
+    <a class="button" href="/updates">Updates</a>
     <a class="button" href="/gallery">Gallery</a>
     <a class="button" href="/contact">Contact</a>
     <a class="button" href="/daily-song">Daily song</a>
