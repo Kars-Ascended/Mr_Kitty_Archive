4e97c60c8db611f922f6aac7ac7eb5a17392342b
Josh White <whitejosh429@gmail.com>
2025-03-08 21:32:24 +0000

    legal stuff added

--- pages/contact.php
+++ pages/contact.php
@@ -6,8 +6,8 @@
 </head>
 <body>
     <div class="main">
-        Affiliates with <a href="https://reddit.com/r/MrKittyFans" target="_blank">r/MrKittyFans</a> on reddit, contact u/FatherEnrico for anything related to the website.
-
+        <p>Affiliates with <a href="https://reddit.com/r/MrKittyFans" target="_blank">r/MrKittyFans</a> on reddit, contact u/FatherEnrico for anything related to the website.</p>
+        <p>This site is non-profit and is not affiliated with Mr.Kitty in any way and bla bla bla and all other legal mumbo, all legal issues related to this site can be dealt with <a href="https://youtu.be/dQw4w9WgXcQ">here</a>.</p>
     </div>
 </body>
 </html>
