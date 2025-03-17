416d5e2fb5ed78356a0beaeebb575afe8147b1ae
Josh White <whitejosh429@gmail.com>
2025-03-17 16:45:53 +0000

    css edits

--- pages/index.php
+++ pages/index.php
@@ -11,6 +11,8 @@
         <h3 class="center" style="color:#ababab;">(<b>M</b>r. <b>K</b>itty <b>A</b>rchive)</h3>
         
         <p>No home page as of now so ill just write some useful information</p>
+        <br>
+        
         - Since this site gets updates alot you might need to use CTRL+F5 to refresh the cache if things are missing/broken<br>
         - 'Pre-2010' refers to albums forrest made before 2010, these do contain heavy breakcore so if you dont like that i recommend being careful in that area<br>
         - The <img class="explicit-icon" src="/assets/sotd_icons/explicit.png"> icon is used to mark explicit content and the <img class="volume-icon" src="/assets/sotd_icons/volume.png"> icon is used to mark loud content such as breakcore however not all breakcore has been tagged yet<br>
