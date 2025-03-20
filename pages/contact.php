20776c0619ee364db806907644f9a7e64d527b09
Josh White <whitejosh429@gmail.com>
2025-03-20 18:24:02 +0000

    contact update

--- pages/contact.php
+++ pages/contact.php
@@ -6,7 +6,8 @@
 </head>
 <body>
     <div class="main">
-        <p>Affiliates with <a href="https://reddit.com/r/MrKittyFans" target="_blank">r/MrKittyFans</a> on reddit, contact u/FatherEnrico for anything related to the website.</p>
+        <p>Affiliates with <a href="https://reddit.com/r/MrKittyFans" target="_blank">r/MrKittyFans Subreddit</a>, <a href="https://discord.gg/H8MWmdkGcZ" target="_blank">MrKittyFans Discord</a>, And ofcourse us here at <a href="" target="_blank">[insert url here]</a></p>
+        <br>
         <p>This site is non-profit and is not affiliated with Mr.Kitty in any way and bla bla bla and all other legal mumbo, all legal issues related to this site can be dealt with <a href="https://youtu.be/dQw4w9WgXcQ">here</a>.</p>
     </div>
 </body>
